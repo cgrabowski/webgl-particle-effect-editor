44c5751a5f69fa41ec3521c700ed7ae270d12daa
Christopher Grabowski <yourchristopher6334@gmail.com>
2013-06-29 18:24:06 -0400

    Opts are stored in browser localStorage on unload and passed to the engine on load.

--- js/gui.js
+++ js/gui.js
@@ -1,211 +1,234 @@
-$(document).ready(function () {
-  $('canvas').width(($(document).width() < 1000) ? $(document).width() : 1000)
-  engine(function (effect) {
+(function () {
 
-    var defaultReq = new XMLHttpRequest()
+  $(document).ready(function () {
+    var opts = localStorage.getItem('opts') || null
+    opts = (opts.match(/{}/)) ? null : JSON.parse(opts)    
+    if (opts)
+      opts.unshift({name: 'shared'})
+    console.log(opts)
+    $('canvas').width(($(document).width() < 1000) ? $(document).width() : 1000)
 
-    defaultReq.onload = function () {
-      handleRes(this.responseText)
-    }
-    defaultReq.open('get', 'http://localhost/WebGLParticleEffectEditor/gui-default.json')
-    defaultReq.send()
+    engine(opts, function (effect) {
+      var defaultReq = new XMLHttpRequest()
 
-
-    function handleRes (res) {
-      var tbOpts = JSON.parse(res)
-      for (var i = 0; i < effect.emitters.length; i++) {
-        var opts = {}
-        for (var opt in tbOpts) {
-          if (opt.match(/name|continuous|wind|rotation vec/))
-            continue
-          opts[opt] = {}
-          for (var val in tbOpts[opt]) {
-            opts[opt][val] = new Array(3)
-            opts[opt][val][0] = tbOpts[opt][val][0]
-            opts[opt][val][1] = effect.emitters[i].opts[val]
-            opts[opt][val][2] = tbOpts[opt][val][1]
-          }
-        }
-        toolbar(effect.emitters[i], opts)
+      defaultReq.onload = function () {
+        handleRes(this.responseText)
       }
-      toolbar(effect.emitters, opts, true)
+      defaultReq.open('get', 'http://localhost/WebGLParticleEffectEditor/gui-default.json')
+      defaultReq.send()
+
+
+      function handleRes (res) {
+        var tbOpts = JSON.parse(res)
+        for (var i = 0; i < effect.emitters.length; i++) {
+          var opts = {}
+          for (var opt in tbOpts) {
+            if (opt.match(/name|continuous|wind|rotation vec/))
+              continue
+            opts[opt] = {}
+            for (var val in tbOpts[opt]) {
+              opts[opt][val] = new Array(3)
+              opts[opt][val][0] = tbOpts[opt][val][0]
+              opts[opt][val][1] = effect.emitters[i].opts[val]
+              opts[opt][val][2] = tbOpts[opt][val][1]
+            }
+          }
+          toolbar(effect.emitters[i], opts)
+        }
+        toolbar(effect.emitters, opts, true)
+      }
+    })
+  })
+
+  $(window).on('resize load', function (event) {
+    var tainer = $('#container')
+    tainer.height($(window).height())
+    tainer.width($(window).width())
+  })
+
+  $(window).on('unload', function (event) {
+    var emitters = $('.toolbar').first().data().emitter.effect.emitters
+    var optsArray = []
+    for (var i = 0; i < emitters.length; i++) {
+      optsArray.push(emitters[i].opts)
     }
-  })
-})
-
-$(window).on('resize load', function (event) {
-  var tainer = $('#container')
-  tainer.height($(window).height())
-  tainer.width($(window).width())
-})
-
-function toolbar (emitter, opts, master) {
-  var tb = $('<div>')
-
-  tb.data('emitter', emitter)
-
-  tb.draggable({snap: '#header, #container, .toolbar'})
-    .addClass('toolbar')
-    .appendTo('#container')
-    .css({
-    position: 'absolute',
-    right: '0px',
-    top: $('header').height()
+    localStorage.setItem('opts', JSON.stringify(optsArray))
   })
 
-    //.resizable({handles: "s, e, w", minWidth: 150})      
+  function toolbar (emitter, opts, master) {
+    var tb = $('<div>')
 
-    .mousedown(function (event) {
-    var self = this
-      , zStack = toolbar.zStack
+    tb.data('emitter', emitter)
 
-    zStack.forEach(function (val, ind, arr) {
-      if (zStack[ind] === self)
-        zStack.splice(ind, 1)
-      else
-        $(zStack[ind]).css('z-index', $(zStack[ind]).css('z-index') - 1)
+    tb.draggable({snap: '#header, #container, .toolbar'})
+      .addClass('toolbar')
+      .appendTo('#container')
+      .css({
+      position: 'absolute',
+      right: '0px',
+      top: $('header').height()
     })
 
-    $(this).css('z-index', 100)
-    zStack.unshift(this)
-    if (zStack.length > $('.toolbar').length)
-      zStack.pop()
-  })
+      //.resizable({handles: "s, e, w", minWidth: 150})      
 
-  $('<div class="toolbar-header">' + ((master) ? "master" : emitter.name) +
-    '<a href="" class="close"><img src="images/gui-close-grey.png"></a>' +
-    '<a href="" class="up"><img src="images/gui-up-grey.png">' +
-    '<a href="" style="display: none" class="down"><img src="images/gui-down-grey.png"></a>' +
-    '</div>').appendTo(tb)
+      .mousedown(function (event) {
+      var self = this
+        , zStack = toolbar.zStack
 
-  $('.up').click(function (event) {
-    var $this = $(this)
-    $this.closest(".toolbar").find('.setting-tainer').css('display', 'none')
-    $this.css('display', 'none').siblings('.down').css('display', 'inline')
-    return false
-  })
+      zStack.forEach(function (val, ind, arr) {
+        if (zStack[ind] === self)
+          zStack.splice(ind, 1)
+        else
+          $(zStack[ind]).css('z-index', $(zStack[ind]).css('z-index') - 1)
+      })
 
-  $('.down').click(function (event) {
-    var $this = $(this)
-    $this.closest(".toolbar").find('.setting-tainer').css('display', 'block')
-    $this.css('display', 'none').siblings('.up').css('display', 'inline')
-    return false
-  })
+      $(this).css('z-index', 100)
+      zStack.unshift(this)
+      if (zStack.length > $('.toolbar').length)
+        zStack.pop()
+    })
 
-  $('.close').click(function (event) {
-    $(this).closest(".toolbar").remove()
-    return false
-  })
+    $('<div class="toolbar-header">' + ((master) ? "master" : emitter.name) +
+      '<a href="" class="close"><img src="images/gui-close-grey.png"></a>' +
+      '<a href="" class="up"><img src="images/gui-up-grey.png">' +
+      '<a href="" style="display: none" class="down"><img src="images/gui-down-grey.png"></a>' +
+      '</div>').appendTo(tb)
 
-  for (var opt in opts) {
-    var min = null
-      , max = null
-      , minKey = null
-      , maxKey = null
-      , val = null
-    for (var key in opts[opt]) {
-      if (key.match(/min/i)) {
-        min = opts[opt][key]
-        minKey = key
-      } else if (key.match(/max/i)) {
-        max = opts[opt][key]
-        maxKey = key
-      } else {
-        val = opts[opt][key]
-        key = [key]
-        break
+    $('.up').click(function (event) {
+      var $this = $(this)
+      $this.closest(".toolbar").find('.setting-tainer').css('display', 'none')
+      $this.css('display', 'none').siblings('.down').css('display', 'inline')
+      return false
+    })
+
+    $('.down').click(function (event) {
+      var $this = $(this)
+      $this.closest(".toolbar").find('.setting-tainer').css('display', 'block')
+      $this.css('display', 'none').siblings('.up').css('display', 'inline')
+      return false
+    })
+
+    $('.close').click(function (event) {
+      $(this).closest(".toolbar").remove()
+      return false
+    })
+
+    for (var opt in opts) {
+      var min = null
+        , max = null
+        , minKey = null
+        , maxKey = null
+        , val = null
+      for (var key in opts[opt]) {
+        if (key.match(/min/i)) {
+          min = opts[opt][key]
+          minKey = key
+        } else if (key.match(/max/i)) {
+          max = opts[opt][key]
+          maxKey = key
+        } else {
+          val = opts[opt][key]
+          key = [key]
+          break
+        }
+
+        if (min && max) {
+          key = [minKey, maxKey]
+          break
+        }
       }
+
+      var settingTainer = $('<div>')
+        , sliderTainer = $('<div>')
+        , minSpan = $('<span class="min-span">')
+        , maxSpan = $('<span class="max-span">')
+
+      settingTainer.addClass('setting-tainer').data('key', key)
+        .appendTo(tb)
+      $('<h5>').text(opt).appendTo(settingTainer)
 
       if (min && max) {
-        key = [minKey, maxKey]
-        break
+        sliderTainer.slider({
+          min: min[0],
+          values: [min[1], max[1]],
+          max: max[2],
+          range: true,
+          step: (max[2] - min[0]) / 1000,
+          slide: function (event, ui) {
+            var emitter = $(ui.handle).closest('.toolbar').data().emitter
+              , tainer = $(ui.handle).closest('.setting-tainer')
+              , key = tainer.data().key
+
+            if (master) {
+              for (var i = 0; i < emitter.length; i++) {
+                emitter[i][key[0]] = ui.values[0]
+                emitter[i].opts[key[0]]  = emitter[i][key[0]]
+                emitter[i][key[1]] = ui.values[1]
+                emitter[i].opts[key[1]]  = emitter[i][key[1]]
+              }
+            } else {
+              emitter[key[0]] = ui.values[0]
+              emitter.opts[key[0]]  = emitter[key[0]]
+              emitter[key[1]] = ui.values[1]
+              emitter.opts[key[1]]  = emitter[key[1]]
+            }
+            tainer.find('a:nth-child(2)').attr('title', ui.values[0])
+            tainer.find('a:last-child').attr('title', ui.values[1])
+          }
+        })
+          .appendTo(settingTainer)
+        settingTainer.find('.ui-slider .ui-slider-handle:nth-child(2)').attr('title', min[1])
+        settingTainer.find('.ui-slider .ui-slider-handle:last-child').attr('title', max[1])
+        minSpan.text(min[0])
+        maxSpan.text(max[2])
+
+      } else {
+        sliderTainer.slider({
+          min: val[0],
+          value: val[1],
+          max: val[2],
+          step: (val[2] - val[0]) / 1000,
+          slide: function (event, ui) {
+            var emitter = $(ui.handle).closest('.toolbar').data().emitter
+              , key = $(ui.handle).closest('.setting-tainer').data().key
+            if (master) {
+              for (var i = 0; i < emitter.length; i++) {
+                emitter[i][key[0]] = ui.value
+                emitter[i].opts[key[0]]  = emitter[i][key[0]]
+              }
+            } else {
+              emitter[key] = ui.value
+              emitter.opts[key]  = emitter[key]
+            }
+            $(ui.handle).attr('title', ui.value)
+          }
+        })
+          .appendTo(settingTainer)
+        settingTainer.find('.ui-slider .ui-slider-handle:last-child').attr('title', val[1])
+        minSpan.text(val[0])
+        maxSpan.text(val[2])
       }
+
+      sliderTainer.closest('.setting-tainer').prepend(minSpan).prepend(maxSpan)
     }
 
-    var settingTainer = $('<div>')
-      , sliderTainer = $('<div>')
-      , minSpan = $('<span class="min-span">')
-      , maxSpan = $('<span class="max-span">')
+    tb.mCustomScrollbar({
+      advanced: {
+        updateOnContentResize: true
+      }})
 
-    settingTainer.addClass('setting-tainer').data('key', key)
-      .appendTo(tb)
-    $('<h5>').text(opt).appendTo(settingTainer)
+    tb.find('.setting-tainer').first().css('padding-top', '15px')
+    tb.find('.setting-tainer').last().css('padding-bottom', '5px')
+    tb.resizable({
+      handles: 's, e, w',
+      minWidth: 175,
+      resize: function (event, ui) {
+        ui.element.find('.toolbar-header').width(tb.width() - (12))
+      }})
 
-    if (min && max) {
-      sliderTainer.slider({
-        min: min[0],
-        values: [min[1], max[1]],
-        max: max[2],
-        range: true,
-        step: (max[2] - min[0]) / 1000,
-        slide: function (event, ui) {
-          var emitter = $(ui.handle).closest('.toolbar').data().emitter
-            , tainer = $(ui.handle).closest('.setting-tainer')
-            , key = tainer.data().key
-
-          if (master) {
-            for (var i = 0; i < emitter.length; i++) {
-              emitter[i][key[0]] = ui.values[0]
-              emitter[i][key[1]] = ui.values[1]
-            }
-          } else {
-            emitter[key[0]] = ui.values[0]
-            emitter[key[1]] = ui.values[1]
-          }
-          tainer.find('a:nth-child(2)').attr('title', ui.values[0])
-          tainer.find('a:last-child').attr('title', ui.values[1])
-        }
-      })
-        .appendTo(settingTainer)
-      settingTainer.find('.ui-slider .ui-slider-handle:nth-child(2)').attr('title', min[1])
-      settingTainer.find('.ui-slider .ui-slider-handle:last-child').attr('title', max[1])
-      minSpan.text(min[0])
-      maxSpan.text(max[2])
-
-    } else {
-      sliderTainer.slider({
-        min: val[0],
-        value: val[1],
-        max: val[2],
-        step: (val[2] - val[0]) / 1000,
-        slide: function (event, ui) {
-          var emitter = $(ui.handle).closest('.toolbar').data().emitter
-            , key = $(ui.handle).closest('.setting-tainer').data().key
-          if (master) {
-            for (var i = 0; i < emitter.length; i++) {
-              emitter[i][key[0]] = ui.value
-            }
-          } else {
-            emitter[key] = ui.value
-          }
-          $(ui.handle).attr('title', ui.value)
-        }
-      })
-        .appendTo(settingTainer)
-      settingTainer.find('.ui-slider .ui-slider-handle:last-child').attr('title', val[1])
-      minSpan.text(val[0])
-      maxSpan.text(val[2])
-    }
-
-    sliderTainer.closest('.setting-tainer').prepend(minSpan).prepend(maxSpan)
+    tb.find('.toolbar-header').width(tb.width() - (12))
+    tb.find('.mCSB_draggerContainer').css('top', '35px')
   }
 
-  tb.mCustomScrollbar({
-    advanced: {
-      updateOnContentResize: true
-    }})
-
-  tb.find('.setting-tainer').first().css('padding-top', '15px')
-  tb.find('.setting-tainer').last().css('padding-bottom', '5px')
-  tb.resizable({
-    handles: 's, e, w',
-    minWidth: 175,
-    resize: function (event, ui) {
-      ui.element.find('.toolbar-header').width(tb.width() - (12))
-    }})
-
-  tb.find('.toolbar-header').width(tb.width() - (12))
-  tb.find('.mCSB_draggerContainer').css('top', '35px')
-}
-
-toolbar.zStack = []
+  toolbar.zStack = []
+}())
